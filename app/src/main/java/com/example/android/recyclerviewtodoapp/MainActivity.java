4829e9d514d688bd6c41ced041fc4c5d741887c1
Rob Hess <robwhess@gmail.com>
2019-01-22 15:44:56 -0800

    Add code from 1/22 lecture.

--- app/src/main/java/com/example/android/recyclerviewtodoapp/MainActivity.java
+++ app/src/main/java/com/example/android/recyclerviewtodoapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.recyclerviewtodoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -12,19 +14,24 @@ import java.util.ArrayDeque;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTodoListTV;
+    private RecyclerView mTodoListRV;
     private EditText mTodoEntryET;
-    private ArrayDeque<String> mTodoList;
+    private TodoAdapter mTodoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTodoList = new ArrayDeque<String>();
-
-//        mTodoListTV = findViewById(R.id.tv_todo_list);
+        mTodoListRV = findViewById(R.id.rv_todo_list);
         mTodoEntryET = findViewById(R.id.et_todo_entry);
+
+        mTodoAdapter = new TodoAdapter();
+
+        mTodoListRV.setLayoutManager(new LinearLayoutManager(this));
+        mTodoListRV.setHasFixedSize(true);
+
+        mTodoListRV.setAdapter(mTodoAdapter);
 
         Button addTodoButton = findViewById(R.id.btn_add_todo);
         addTodoButton.setOnClickListener(new View.OnClickListener() {
@@ -32,11 +39,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String todoText = mTodoEntryET.getText().toString();
                 if (!TextUtils.isEmpty(todoText)) {
-                    mTodoList.push(todoText);
-                    mTodoListTV.setText("");
-                    for (String todo : mTodoList) {
-                        mTodoListTV.append(todo + "\n\n");
-                    }
+                    mTodoAdapter.addTodo(todoText);
                     mTodoEntryET.setText("");
                 }
             }
